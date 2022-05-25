aeeb46ba49cb8c39329ee92f150503584cb48103
kjes9307 <kjes9307@gmail.com>
2022-05-25 18:54:42 +0800

    add swagger api

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require("dotenv");
+const swaggerUI = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json")
 const {errorResponse,appError,errorResponseDEV} = require('./util/tool')
 require('./connect')
 dotenv.config({path :"./config.env"});
@@ -22,13 +24,20 @@ require('./util/exceptionHandle')
 // app.use('/', indexRouter);
 app.use('/posts', postRouter);
 app.use('/user', userRouter);
+app.use('/api-doc',swaggerUI.serve,swaggerUI.setup(swaggerFile));
 
 app.use((req,res,next)=>{
+    /**
+     * #swagger.ignore = true
+     */
     console.log("@404路由");
     appError(404,"URL Not Found",next,res)
 })
 
 app.use((err,req,res,next)=>{
+    /**
+     * #swagger.ignore = true
+     */
     const {name,statusCode}=err;
     console.log("Error Manager",name,statusCode)
     if(process.env.NODE_ENV==='dev'){
--- routes/posts.js
+++ routes/posts.js
@@ -6,22 +6,101 @@ const postAPI= require("../controller/postMethod.js")
 
 /* GET home page. */
 router.get('/',isAuth,asyncErrorHandler(async function(req, res, next) {
+    /**
+     * #swagger.tags=['Post'] 
+     * #swagger.description = '取得貼文'
+     * #swagger.security = [{
+        'apiAuth' : [] 
+       }]
+     * #swagger.parameters['body'] = {
+     
+       }
+     * #swagger.responses[200] = {
+            description : 'some api info',
+            schema: {
+                'status' : true,
+                'data': [
+                    {
+                        '_id': '628dbcf49adbd24a2c962de1',
+                        'name': '大帥哥中的大帥哥',
+                        'user': {
+                            '_id': '6283a064261ad96649ea9d05',
+                            'name': '大帥哥中的大帥哥',
+                            'photo': 'https://i.imgur.com/JhstGJB.png'
+                        },
+                        'tags': '["心情"]',
+                        'type': 'group',
+                        'image': 'https://i.imgur.com/q3NHdNn.jpg',
+                        'content': 'test',
+                        'likes': 0,
+                        'comments': 0,
+                        'createAt': '2022-05-25T05:21:56.851Z'
+                    }
+                ]
+            }
+        }
+     */
     await postAPI.findPost({req,res,next});
 }));
 
 router.post('/',isAuth,asyncErrorHandler(async function(req, res, next) {
+    /**
+     * #swagger.tags=['Post'] 
+     * #swagger.description = '新增貼文'
+     * #swagger.security = [{
+        'apiAuth' : [] 
+       }]
+     * #swagger.parameters['body'] = {
+            in : 'body',
+            type: 'object',
+            required : true,
+            description: '格式',
+            schema : {
+            '$content': '你的小老虎上限拉',
+            '$tags' : '心情',
+            '$type' : 'group'
+            }
+       }
+     * #swagger.responses[200] = {
+            description : 'add',
+            schema: {
+                'status' : true,
+                'data': [
+                    {
+                        '_id': '628dbcf49adbd24a2c962de1',
+                        'name': '大帥哥中的大帥哥',
+                        'user': {
+                            '_id': '6283a064261ad96649ea9d05',
+                            'name': '大帥哥中的大帥哥',
+                            'photo': 'https://i.imgur.com/JhstGJB.png'
+                        },
+                        'tags': '["心情"]',
+                        'type': 'group',
+                        'image': 'https://i.imgur.com/q3NHdNn.jpg',
+                        'content': 'test',
+                        'likes': 0,
+                        'comments': 0,
+                        'createAt': '2022-05-25T05:21:56.851Z'
+                    }
+                ]
+            }
+        }
+     */
     await postAPI.createPost({req,res,next});    
 }));
 
 router.delete('/:id',asyncErrorHandler(async function(req, res, next) {
+
     await postAPI.deleteByID({req,res,next})
 }));
 
 router.delete('/',asyncErrorHandler(async function(req, res, next) {
+
     await postAPI.deleteAll({req,res,next})
 }));
 
 router.patch('/:id',asyncErrorHandler(async function(req, res, next) {
+ 
     await postAPI.editPost({req,res,next})
 }));
 
